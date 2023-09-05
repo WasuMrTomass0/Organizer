0aa20f003a2b0d63d4f109bd0c69776fe68f13a1
Wasu PC Gaming <tomasz_moleda@wp.pl>
2023-09-05 18:54:31 +0200

    Delete obsolete code

--- src/gui/common.py
+++ src/gui/common.py
@@ -69,15 +69,3 @@ def process_image(image: bytes, dim: tuple = (1024,1024), preserve_ratio: bool =
 
 def image_to_base64(image: bytes) -> str:
     return f'data:image/jpeg;base64,{base64.b64encode(image).decode("ascii")}'
-
-
-def create_dialog_delete_back(label: str = None) -> ui.dialog:
-    # Question / Sentence displayed
-    label = label if label else 'Are you sure?'
-    # Widgets
-    with ui.dialog(value=False) as dialog, ui.card():
-        ui.label(label)
-        with ui.row().classes('w-full no-wrap'):
-            ui.button('Back', on_click=lambda: dialog.submit(False)).classes('w-1/2')
-            ui.button('Delete', color='red', on_click=lambda: dialog.submit(True)).classes('w-1/2')
-    return dialog
