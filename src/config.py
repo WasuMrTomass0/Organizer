a99cb550254bfea0c9ec182511ab6117666e7d47
Wasu PC Gaming <tomasz_moleda@wp.pl>
2023-09-15 15:20:08 +0200

    Fix paths to images

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import os
+
+
+DIR_SRC = os.path.dirname(__file__)
+DIR_DATA = os.path.join(DIR_SRC, 'data')
+DIR_DOC = os.path.join(DIR_SRC, 'doc')
+
+FILE_IMAGE_DEFAULT = os.path.join(DIR_DATA, 'no_photo.jpg')
+FILE_IMAGE_LOGO = os.path.join(DIR_DATA, 'organizer_logo.png')
