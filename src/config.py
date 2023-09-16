d3937510112f5d23b59591657ae3207a4fed21ef
Wasu PC Gaming <tomasz_moleda@wp.pl>
2023-09-16 21:10:14 +0200

    Add flag images

--- src/config.py
+++ src/config.py
@@ -9,4 +9,6 @@ DIR_DOC = os.path.join(DIR_ROOT, 'doc')
 
 FILE_IMAGE_DEFAULT = os.path.join(DIR_DATA, 'no_photo.jpg')
 FILE_IMAGE_LOGO = os.path.join(DIR_DATA, 'organizer_logo.png')
+FILE_IMAGE_FLAG_PL = os.path.join(DIR_DATA, 'flag_polish.jpg')
+FILE_IMAGE_FLAG_ENG = os.path.join(DIR_DATA, 'flag_english.jpg')
 FILE_SECRETS = os.path.join(DIR_DATA, 'secrets.json')
