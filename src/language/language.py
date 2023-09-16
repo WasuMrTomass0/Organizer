8bb4b32938ae6cdc2ae45d40087f801812d5ae49
Wasu PC Gaming <tomasz_moleda@wp.pl>
2023-09-16 20:29:13 +0200

    Update language - polish

--- src/language/language.py
+++ src/language/language.py
@@ -2,20 +2,45 @@ import json
 import os
 
 from config import DIR_LANGUAGE
-from logger import warning
+from logger import warning, error
 
 
 class Language:
 
     def __init__(self, language: str = None) -> None:
         self._language = language if language else 'english'
-        path = os.path.join(DIR_LANGUAGE, f'{self._language}.json')
+        self._path = None
         self._data = {}
-        self._load_data(path=path)
+        self._load_language()
+
+    def _set_path(self) -> None:
+        self._path = os.path.join(DIR_LANGUAGE, f'{self._language}.json')
+
+    def _is_language(self) -> bool:
+        if self._path is None:
+            return False
+        return os.path.isfile(self._path)
 
     def _load_data(self, path):
-        with open(path, 'r') as f:
+        with open(path, 'r', encoding='utf-8') as f:
             self._data = json.load(f)
+
+    def _load_language(self, raise_err: bool = False) -> None:
+        # Set file path based on selected language
+        self._set_path()
+        # Check if file exists
+        if not self._is_language():
+            # Log error message
+            msg = f'Language data file does not exist! "{self._path}"'
+            error(msg)
+            # If called again - raise error
+            if raise_err:
+                raise FileNotFoundError(msg)
+            # Set default language
+            self._language = 'english'
+            return self._load_language(raise_err=True)
+        # Load existing file
+        self._load_data(path=self._path)
 
     def __getattr__(self, name) -> str:
         if name in self._data:
